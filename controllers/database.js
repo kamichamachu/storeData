45d5bac233a2fb64ad3f131be82a658644dd25c8
kamichamachu <gharvie@horizon.csueastbay.edu>
2018-05-17 09:32:32 -0700

    Make it Better

--- controllers/database.js
+++ controllers/database.js
@@ -31,7 +31,7 @@ module.exports.getAllOrders =  function (request, response) {
         });
 
         //close connection when your app is terminating.
-        db.close(function (err) {
+        theDatabase.close(function (err) {
             if(err) throw err;
         });
     });//end of connect
