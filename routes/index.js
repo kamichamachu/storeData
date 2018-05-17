5aeec7f5f2ea89f46486e1db23b8edd82a5f34d8
kamichamachu <gharvie@horizon.csueastbay.edu>
2018-05-17 09:00:10 -0700

    Make it Better

--- routes/index.js
+++ routes/index.js
@@ -14,3 +14,20 @@ router.post('/storeData', function(req, res, next) {
 var value_name = req.body.order  //retrieve the data associated with order
 res.send("order succesfully received: " + value_name);
 });
+
+
+var express = require('express');
+var router = express.Router();
+
+//LOAD the various controllers
+var controllerMain = require('../controllers/main');   //this will load the main controller file
+var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+
+//MAY HAVE OTHER CODE in index.js
+
+
+//CODE to route /getAllRoutes to appropriate  Controller function
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection w
+//      and Render information iwith an ejs view
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
