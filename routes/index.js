a7a653ba88a05a7f08a5e140536f50740b505912
kamichamachu <gharvie@horizon.csueastbay.edu>
2018-05-17 08:55:00 -0700

    Make it Better

--- routes/index.js
+++ routes/index.js
@@ -14,39 +14,3 @@ router.post('/storeData', function(req, res, next) {
 var value_name = req.body.order  //retrieve the data associated with order
 res.send("order succesfully received: " + value_name);
 });
-
-var express = require('express');
-var XXX = express.Router();
-var mongodb = require('mongodb');
-var mongoDBURI = process.env.MONGODB_URI || 'mongodb://glharvie:nRl40HM8!@ds225840.mlab.com:25840/heroku_mkwg2k9h';
-
-
-
-//**************************************************************************
-//***** mongodb get all of the Routes in Routes collection where frequence>=1
-//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
-XXX.get('/mongodb', function (request, response) {
-
-    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
-        if(err) throw err;
-        //get handle to the database
-        var theDatabase = client.db('heroku_mkwg2k9h');
-
-
-        //get collection of Routes
-        var Routes = db.collection('Routes');
-        //get all Routes
-        Routes.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('pages/mongodb', {results: docs});
-
-        });
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-    });//end of connect
-
-});//end XXX.get
