65457633b4472b703eaf648d864b91306eb1494b
kamichamachu <gharvie@horizon.csueastbay.edu>
2018-05-17 09:25:50 -0700

    Make it Better

--- routes/index.js
+++ routes/index.js
@@ -16,14 +16,10 @@ res.send("order succesfully received: " + value_name);
 });
 
 
-var express = require('express');
-var router = express.Router();
-
 //LOAD the various controllers
 var controllerMain = require('../controllers/main');   //this will load the main controller file
 var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
 
-//MAY HAVE OTHER CODE in index.js
 
 
 //CODE to route /getAllRoutes to appropriate  Controller function
